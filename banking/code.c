3903011c9dbb224fa5f02ae6b41e9cebc681d540
Lena Heimberger <S1510239012@students.fh-hagenberg.at>
2018-03-30 14:55:35 +0200

    init repo

---
+++ banking/code.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <getopt.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <limits.h>
+
+void printer(){
+    printf("INT_MIN= %d\n", INT_MIN);
+    printf("INT_MAX= %d\n", INT_MAX);
+}
+
+int get_balance(int account){//null pointer
+    
+}
+
+int add_account(int account){ //buffer overflow
+
+}
+
+int withdraw(int amount, int account){ //integer overflow
+
+}
+
+int deposit(int amount, int accound){
+
+}
+
+int main(int argc, char* argv){
+
+
+}
