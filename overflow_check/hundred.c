d22d6a2936ad5d321d6ca6b0c2dfab965699b1cb
Lena Heimberger <S1510239012@students.fh-hagenberg.at>
2018-04-12 14:16:57 +0200

    overflow simplification example

---
+++ overflow_check/hundred.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void handle_overflow(){
+    exit(-1); 
+}
+
+void add_x(int x){
+    if((x+100)<x){
+        puts("this is too large"); 
+        handle_overflow(); 
+    }
+    printf("The nr is %d\n", x+100); 
+
+}
+
+int main(int argc, char *argv[]){
+    if(argc != 2){
+        puts("Please supply a starting value!");
+        return 1;
+    }
+    int x=atoi(argv[1]);
+    add_x(x); 
+    return 0; 
+}
